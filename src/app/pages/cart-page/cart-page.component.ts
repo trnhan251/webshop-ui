13e8eba7d68a292d4d5b50a48163ff225b150223
Nhan Vu <nhan997@gmail.com>
2020-07-16 15:16:46 +0200

    fix cost

--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -12,9 +12,20 @@ import * as CartActions from './store/cart.actions';
 })
 export class CartPageComponent implements OnInit {
   public cartOrders: CartOrder[];
+  public products: Product[];
+  public totalCost: number;
+  public shippingCost: number;
+
   constructor(private store: Store<fromApp.AppState>) {
     this.store.select('cart').subscribe(cartState => {
       this.cartOrders = cartState.cartOrders;
+      this.products = cartState.products;
+      this.totalCost = 0;
+      this.cartOrders.forEach(cartOrder => {
+         const product = this.products.find(p => p.id === cartOrder.productId);
+         this.totalCost = this.totalCost + (cartOrder.quantity * product.price);
+      });
+      this.shippingCost = (this.totalCost > 100) ? 0 : 10;
     });
     if (localStorage.getItem('sessionId') != null) {
       this.store.dispatch(new CartActions.CartLoadStart());
